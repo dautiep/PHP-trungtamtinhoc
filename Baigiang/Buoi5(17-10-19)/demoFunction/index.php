141eef335af058676dbebc44e6f51b20069f0223
dautiep <dautiep123@gmail.com>
2019-10-17 20:43:12 +0700

    upload-ver1.3

--- Baigiang/Buoi5(17-10-19)/demoFunction/index.php
+++ Baigiang/Buoi5(17-10-19)/demoFunction/index.php
@@ -55,6 +55,82 @@
     Tong_static(); //goi lần đầu tiên $a = 1
     Tong_static(); //gọi lần thứ hai $a = 2
     Tong_static(); //gọi lần thứ ba $a = 3
+    echo '<hr>';
+
+    //Truyền tham trị và tham chiếu
+    //Tham trị
+    echo 'truyền tham trị <br>';
+    function Hoan_vi($a, $b){ //truyền vào giá trị của $a và $b nên khi kết thúc hàm thì giá trị của hai biến vẫn được giữ nguyên
+        $tam = $a;
+        $a = $b;
+        $b = $tam;
+        echo 'Trong khi hoán vị: $a = '.$a.'  $b = '.$b.'<br>';    
+    }
+    $a = 100;
+    $b = 200;
+    echo 'Trước hoán vị: $a = '.$a.'  $b = '.$b.'<br>';
+    Hoan_vi($a, $b);
+    echo 'Sau hoán vị: $a = '.$a.'  $b = '.$b.'<br>';
+
+    //Tham biến
+    echo '<br> <br> truyền tham biến <br>';
+    function Hoan_vi1(&$a, &$b){ //truyền vào địa chỉ của $a và $b nên khi kết thúc hàm giá trị của hai biến bị thay đổi theo
+        $tam = $a;
+        $a = $b;
+        $b = $tam;
+        echo 'Trong khi hoán vị: $a = '.$a.'  $b = '.$b.'<br>';    
+    }
+    $a = 100;
+    $b = 200;
+    echo 'Trước hoán vị: $a = '.$a.'  $b = '.$b.'<br>';
+    Hoan_vi1($a, $b);
+    echo 'Sau hoán vị: $a = '.$a.'  $b = '.$b.'<hr>';
+
+    //Test các hàm cơ bản trong PHP
+    echo 'Hàm pi:<br>';
+    $r = 3;
+    echo '$s = '.$s = pi() * pow($r, 2).'<br>';
+    echo '$p = '.$p = 2 * pi() * $r;
+
+    echo '<br><br>Hàm trị tuyệt đối:<br>';
+    $r1 = -3;
+    echo 'abs($r1) = '.abs($r1);
+
+    echo '<br><br>Hàm làm tròn:<br>';
+    $r2 = 2.3523;
+    echo '$r2 = 2.3523 sau khi làm tròn là: '.round($r2, 2);
+
+    echo '<br><br>Hàm phát sinh số ngầu nhiên:<br>';
+    echo 'Số ngẫu nhiên trong [1, 100] là: '.rand(1, 100);
+
+    echo '<br><br>Các hàm xử lí chuoi:<br>';
+    $ten = 'Dau   Le Quoc  Tiep  ';
+    echo '"    Dau   Le Quoc  Tiep  "sau khi được loại bỏ khoảng trắng thừa là: '.trim($ten).'<br>';
+    echo 'Độ dài của $ten là: '.strlen($ten).'<br>';
+    $ten1 = 'Dau';
+    $vitri = strpos($ten, $ten1);
+    if($vitri == false)
+        echo 'Không tìm thấy';
+    else
+        echo 'Tìm thấy';
+
+    //Bắt lỗi
+    echo '<hr>';
+    function inverse($x){
+        if(!$x)
+            throw new Exception("Devision by zero");
+        return 1/$x;
+    }
+    try {
+        echo inverse(5);
+        echo inverse(0);
+        echo 'end';
+    } catch (Exception $e) {
+        echo '<br>';
+        echo $e->getMessage();
+    }
+
+
 
 
 ?>
