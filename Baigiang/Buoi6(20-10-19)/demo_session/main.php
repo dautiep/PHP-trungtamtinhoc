4da2ccd0177ce6be37b12d074d92e15e43d683b1
dautiep <dautiep123@gmail.com>
2019-10-20 16:31:47 +0700

    upload(20-10-19)ver2.2

--- Baigiang/Buoi6(20-10-19)/demo_session/main.php
+++ Baigiang/Buoi6(20-10-19)/demo_session/main.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    //kiểm tra xem có đăng nhập trước đó chưa, nếu rồi thì vào trang main, không thì phải đăng nhập 
     if(isset($_SESSION['email']))
         $email = $_SESSION['email'];
     else{
@@ -9,58 +10,30 @@
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <?php require_once 'layout/head.php'; ?>
+    <body>
+    <?php require_once 'layout/nav.php'; ?>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <?php
+        //kiểm tra file có tồn tại hay không, nếu có mở file không thì thông báo lỗi
+        if(file_exists('file_van_ban/ttHV.txt')){
+            $f = fopen('file_van_ban/ttHV.txt', 'r');
+            $chuoiTT = fgets($f);
+            $mangTT = explode('|', $chuoiTT); //tách chuỗi thành mảng, ngăn cách bằng một kí tự bất kì
+            echo '<p>Họ tên học viên: '.$mangTT[0].'</p>';
+            if($mangTT[1] == 'true')
+                echo '<p>Giới tính: Nam</p>';
+            else
+                echo '<p>Giới tính: Nu</p>';
+            echo '<p>Ngày sinh: '.$mangTT[2].'</p>';
+            echo '<p>Email: '.$mangTT[3].'</p>';
 
-    <title>Trang chủ</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Something else here</a>
-            </div>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-        </ul>
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a href="dang_xuat.php" class="nav-link">Chào bạn <?php echo $email; ?></a>
-            </li>
-        </ul>
-    </div>
-    </nav>
-
+            fclose($f);
+        }else
+            echo 'File bạn yêu cầu không tồn tại!';
+    ?> 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+    <?php require_once 'layout/script.php'; ?>
+    </body>
 </html>
